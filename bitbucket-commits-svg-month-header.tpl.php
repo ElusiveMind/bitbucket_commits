4d7aecd444161cb573ea8e7df7ef7ef0b1228b08
Michael Bagnall <mrbagnall@icloud.com>
2016-07-31 08:02:50 -0400

    Added the legend to the top and sides of the activity graph for days and months

---
+++ bitbucket-commits-svg-month-header.tpl.php
@@ -0,0 +1,1 @@
+      <text x="<?php echo $x;?>" y="<?php echo $y;?>" class="<?php echo $class;?>"><?php echo $month;?></text>
--- bitbucket-commits-svg.tpl.php
+++ bitbucket-commits-svg.tpl.php
@@ -1,5 +1,13 @@
 <svg width="<?php echo $width; ?>" height="<?php echo $height; ?>" class="<?php echo $class; ?>">
   <g transform="translate(<?php echo $offset_x;?>, <?php echo $offset_y;?>)">
     <?php echo $weeks; ?>
+    <?php echo $month_header; ?>
+    <text text-anchor="middle" class="wday" dx="-10" dy="9" style="display: none;">S</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="22">M</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="35" style="display: none;">T</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="48">W</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="61" style="display: none;">T</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="74">F</text>
+    <text text-anchor="middle" class="wday" dx="-10" dy="87" style="display: none;">S</text>
   </g>
 </svg>
