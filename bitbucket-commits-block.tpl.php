731b52d736cd4c2b362736013f438968d2f3fdc1
Michael Bagnall <mrbagnall@icloud.com>
2016-08-07 07:29:02 -0400

    Work on our block display and functions for streaks.

---
+++ bitbucket-commits-block.tpl.php
@@ -0,0 +1,7 @@
+<?php echo $graph; ?><br />
+Total Commits: <?php echo $total_commits;?><br />
+Current Streak: <?php echo $current_streak;?><br />
+Longest Streak: <?php echo $max_streak;?><br /><br />
+
+Lines Added: <?php echo number_format($lines_added, 0);?><br /> 
+Lines Removed: <?php echo number_format($lines_removed, 0);?>
